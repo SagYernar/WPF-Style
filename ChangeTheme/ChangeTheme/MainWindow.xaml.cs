ecbcdbc1955184db14450cdd27f9e21e6731c6ad
SagYernar <sagyernar@gmail.com>
2017-10-24 18:32:25 +0600

    1

---
+++ ChangeTheme/ChangeTheme/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeTheme
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Light_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources = new ResourceDictionary { Source = new Uri(@"C:\Users\СагадиевЕ\Documents\Visual Studio 2017\Projects\ChangeTheme\ChangeTheme\LightThemeDictionary.xaml") };
+        }
+        private void Dark_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources = new ResourceDictionary { Source = new Uri(@"C:\Users\СагадиевЕ\Documents\Visual Studio 2017\Projects\ChangeTheme\ChangeTheme\DarkThemeDictionary.xaml") };
+        }
+        private void Mine_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources = new ResourceDictionary { Source = new Uri(@"C:\Users\СагадиевЕ\Documents\Visual Studio 2017\Projects\ChangeTheme\ChangeTheme\mineThemeDictionary.xaml") };
+        }
+    }
+}
